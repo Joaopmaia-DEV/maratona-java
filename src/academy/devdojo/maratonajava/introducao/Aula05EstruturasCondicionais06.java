dd30a3519483981355351bcd7edc9212823d319d
JoaoP <joaopaulolasmarmaaaia@hotmail.com>
2026-03-15 23:49:46 -0300

    Continuações

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais06 {
+    static void main() {
+        // Utilizando switch, dados os valores de 1 a 7, imprima se é dia útil ou final de semana
+        // Considerando 1 como domingo
+        byte dia = 8;
+        switch (dia) {
+            case 1:
+            case 7:
+                System.out.println("Final de semana");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia útil");
+                break;
+            default:
+                System.out.println("Opção Inválida");
+        }
+        switch (dia) {
+            case 1, 7 -> System.out.println("Fim de semana");
+            case 2, 3, 4, 5, 6 -> System.out.println("Dia útil");
+            default -> System.out.println("Opção Inválida");
+        }
+    }
+}
