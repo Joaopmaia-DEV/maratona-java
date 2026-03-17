3082da3018d95902258002ac27227da81b2e6017
Joaopmaia-DEV <csgofreemarquin2@gmail.com>
2026-03-17 15:57:15 -0300

    Changes

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -4,12 +4,12 @@ public class Aula06EstruturasDeRepeticao04 {
     // Dado um valor de um carro, descubra em quantas vezes ele pode ser parcelado
     // Condição  valorParcela >= 1000
     static void main(String[] args) {
-        double valorCarro = 30000;
+        double valorCarro = 1200;
         double valorParcela;
         int parcela = 1;
         for(parcela = 1;; parcela++){
             valorParcela = valorCarro / parcela;
-            if(valorParcela < 1000){
+            if(valorParcela < 300){
                 break;
             }
             System.out.println("Parcela "+parcela+" Valor "+valorParcela);
