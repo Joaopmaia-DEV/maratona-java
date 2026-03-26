ec4629447f59c483f5a695fdae67607ebf5e6276
Joaopmaia-DEV <csgofreemarquin2@gmail.com>
2026-03-25 23:43:38 -0300

    feat: implement method parameters in Java

---
+++ src/academy/devdojo/maratonajava/javacore/Bintrucaoclasses/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Bintrucaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Bintrucaoclasses.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10, 20.5F);
+    }
+}
