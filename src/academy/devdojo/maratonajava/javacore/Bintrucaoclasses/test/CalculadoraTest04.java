10e55dc927dfffcc14975e7907a8eb12f3f040bb
Joaopmaia-DEV <csgofreemarquin2@gmail.com>
2026-03-26 15:41:42 -0300

    feat: add methods with primitive parameters

---
+++ src/academy/devdojo/maratonajava/javacore/Bintrucaoclasses/test/CalculadoraTest04.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Bintrucaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Bintrucaoclasses.dominio.Calculadora;
+
+public class CalculadoraTest04 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        int a = 1;
+        int b = 2;
+        calculadora.alteraDoisNumeros(a, b);
+        System.out.println("\nDentro da CalculadoraTest04\n");
+
+        System.out.println("Num1 "+a);
+        System.out.println("Num2 "+b);
+    }
+}
